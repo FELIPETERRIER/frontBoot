7b575672028caef494c99a6e3bc2c2260ba02a5e
Felipe Bitencourt <felipeterrier@yahoo.com.br>
2025-03-02 10:56:56 -0300

    starting the loginscreen

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { IMaskInput } from "react-imask";
 
 
@@ -9,11 +8,15 @@ const Login = () => {
       <div className="container">
         <h1>Acesse ao sistema</h1>
         <form>
-            <IMaskInput 
+            <div>
+                <IMaskInput 
                 mask="000.000.000-00"
                 placeholder="Digite o seu CPF"
-            />
-            <input type="password" name="password" minLength={6}></input>
+                />
+            </div>
+            <div>
+                <input type="password" name="password" placeholder='Digite sua senha ' minLength={6}/>
+            </div>
         </form>
       </div>
     </div>
