fd21b51c512ca46a6d2dbe52deb5a3589f467bde
Felipe Bitencourt <felipeterrier@yahoo.com.br>
2025-03-02 21:27:37 -0300

    screen login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -8,6 +8,10 @@ const Login = () => {
       <div className="container">
         <h1>Acesse ao sistema</h1>
         <form>
+            
+            <div>
+                <input type="number" name="id" placeholder='Digite seu id ' minLength={1}/>
+            </div>
             <div>
                 <IMaskInput 
                 mask="000.000.000-00"
@@ -17,6 +21,8 @@ const Login = () => {
             <div>
                 <input type="password" name="password" placeholder='Digite sua senha ' minLength={6}/>
             </div>
+            <button>Enter</button>
+            
         </form>
       </div>
     </div>
