80d28a2dd7fecf19e4a40ba9541840a6281a42af
FelipeBdoValle <felipeterrier@yahoo.com.br>
2025-03-06 19:56:34 -0300

    tela login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,28 +1,64 @@
 import React from 'react';
+import './Login.css'
+import { FaUser, FaLock, FaIdBadge } from 'react-icons/fa';
+import { useState } from 'react';
 import { IMaskInput } from "react-imask";
 
 
 const Login = () => {
+  const[id,setId] = useState("");  
+  const[user,setUser] = useState("");
+  const[password,setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); 
+   
+    alert('enviando os dados' + id + " - " + user + " - "  + password)
+
+    console.log('envio')
+  }
+
+
+
   return (
     <div>
       <div className="container">
         <h1>Acesse ao sistema</h1>
-        <form>
-            
-            <div>
-                <input type="number" name="id" placeholder='Digite seu id ' minLength={1}/>
-            </div>
-            <div>
-                <IMaskInput 
-                mask="000.000.000-00"
-                placeholder="Digite o seu CPF"
-                />
-            </div>
-            <div>
-                <input type="password" name="password" placeholder='Digite sua senha ' minLength={6}/>
-            </div>
-            <button>Enter</button>
-            
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input type="number" name="id" placeholder='Digite seu id ' minLength={1}
+            onChange={(e) =>{setId(e.target.value)}} />
+            <FaIdBadge />
+          </div>
+          <div>
+            <IMaskInput className='user' 
+              mask="000.000.000-00"              
+              placeholder="Digite o seu CPF"
+              onChange={(e) => setUser(e.target.value)}
+            />
+            <FaUser />
+          </div>
+          <div>
+            <input type="password" name="password" placeholder='Digite sua senha ' minLength={6} 
+            onChange={(e) =>setPassword(e.target.value) } />
+            <FaLock />
+          </div>
+          <button>Enter</button>
+          <div className='recall-forget'>
+            <label>
+              <input type="checkbox" />
+              Lembre de mim!
+            </label>
+            <a href="#">Esqueceu a senha?</a>
+          </div>
+          <div className='singup-link'>
+            <p>
+              Não tem conta?<a href="#">
+                Criar nova conta.
+              </a>
+            </p>
+          </div>
+
         </form>
       </div>
     </div>
